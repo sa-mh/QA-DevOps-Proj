24763c570e2d814f2026d2fe4afaf7291f267605
Ubuntu <ubuntu@ip-172-31-35-206.eu-west-1.compute.internal>
2020-11-13 16:11:14 +0000

    keys hidden

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
   shared_credentials_file = "/home/ubuntu/.aws/credentials"
 }
 resource "aws_key_pair" "sshKeyGen" {
-  key_name   = "terraKey"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDS3Oy5bAUbmbV+Ne0rvBqJ2ex4+1ubjC5Y/pHPAMKRybpn46d1S779FzJMrc2BOvuUPTOLOrtXPzbFevNfndO136RLHxm1lPd9GnUHbISh4ST6Jms22W6y93bWN/Gqja6wOg2wdYmNmWp2PO23lHf/f1AJn8Ik8CDmyxdpW5sUG5IO5Co8z+9smdqz+W6qaOaohoyIKU43UFzhs54fjKVQGm3k7cspLNRi5BGjn2FQkVyXZV4zRBvYEOm/BpTh4fBTYvAYKA28V1dQNyu4KLZvbFg4U8nqLhgVP196JdAOlgLWlQdyJZYhKJQyHeQHE4XXJBvCcdBXMxCgYZC//6Nz ubuntu@ip-172-31-35-206"
+  key_name   = var.keyName
+  public_key = var.pubKey
 }
 module "ec2" {
   source             = "./ec2"
