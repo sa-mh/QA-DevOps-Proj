56d058ff5665a49a9432406c16512929a865f166
Ubuntu <ubuntu@ip-172-31-35-206.eu-west-1.compute.internal>
2020-11-17 11:50:17 +0000

    no longer returns versions

--- ansible/docker-install.sh
+++ ansible/docker-install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 curl https://get.docker.com | sudo bash
 sudo usermod -aG docker $(whoami)
-docker --version >> versions.txt
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version >> versions.txt
