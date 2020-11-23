b71fcbd952d9a0e8dba6de3a1648595bed753ee9
Ubuntu <ubuntu@ip-172-31-35-206.eu-west-1.compute.internal>
2020-11-23 01:05:52 +0000

    Rollout didnt work, deleting instead

--- ansible/Docker-Compose/run-tests.sh
+++ ansible/Docker-Compose/run-tests.sh
@@ -3,8 +3,8 @@
 docker pull horners/frontend:latest
 docker pull horners/backend:latest
 
-mysql -h terraform-20201122183745034600000003.cml50gxwj1hn.eu-west-1.rds.amazonaws.com -P 3306 -u root -ppassword < database/Create.sql
-mysql -h terraform-20201122183745032700000002.cml50gxwj1hn.eu-west-1.rds.amazonaws.com -P 3306 -u root -ppassword < database/Create.sql
+mysql -h terraform-20201122225022028000000002.cml50gxwj1hn.eu-west-1.rds.amazonaws.com -P 3306 -u root -ppassword < database/Create.sql
+mysql -h terraform-20201122225022034200000003.cml50gxwj1hn.eu-west-1.rds.amazonaws.com -P 3306 -u root -ppassword < database/Create.sql
 
 docker-compose up -d
 docker exec backend bash -c "pytest tests/ --cov application"
